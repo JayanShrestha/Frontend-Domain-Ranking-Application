8af26f43e701696bbf98d85a320d3b1f67210b89
Jayan Shrestha <jayanshr44@gmail.com>
2026-01-09 23:48:01 +1100

    final fix for ranking api

--- src/api/rankingAPI.js
+++ src/api/rankingAPI.js
@@ -1,25 +1,31 @@
 import axios from "axios";
 
-const api = "http://localhost:3000/ranking/tranco";
+//const api = "http://localhost:3000/ranking/tranco";
+
+const API_BASE_URL = import.meta.env.VITE_API_URL;
 
 //for single domain
 export async function fetchsingledomain(domain){
-    const result = await axios.get(api,{
-        params:{
-            domain
-        },
-    });
-   return result.data;
+    //const result = await axios.get(api,{
+      //  params:{
+        //    domain
+        //},
+    //});
+   //return result.data;
+   const response = await fetch(`${API_BASE_URL}/ranking/tranco?domain=${encodeURIComponent(domain)}`);
+  return response.json();
+
 }
 //for multiple domain
 export async function fetchmultdomain(domains){
    // const domainsArray = domains.join(','); backend handles the array conversion of strings
-    const result = await axios.get(api+"/multi",{
-        params:{
-            domains
-        },
-    });
-    console.log(result.data);
-    return result.data;
-    
+    //const result = await axios.get(api+"/multi",{
+        //params:{
+        //    domains
+      //  },
+    //});
+  //  return result.data;
+    const response = await fetch(`${API_BASE_URL}/ranking/tranco/multi?domains=${encodeURIComponent(domains)}`);
+  return response.json();
+
 }
