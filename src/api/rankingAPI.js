5041cb9078ea523256cba686cdf6b8cec93cb881
Jayan Shrestha <jayanshr44@gmail.com>
2026-01-21 22:23:28 +1100

    update for ranking API for cloud flare deployment

--- src/api/rankingAPI.js
+++ src/api/rankingAPI.js
@@ -1,31 +1,15 @@
-import axios from "axios";
 
-const api = "http://localhost:3000/ranking/tranco";
-
-//const API_BASE_URL = import.meta.env.VITE_API_URL;
+const API_BASE_URL = import.meta.env.VITE_API_URL;
 
 //for single domain
 export async function fetchsingledomain(domain){
-    const result = await axios.get(api,{
-        params:{
-            domain
-        },
-    });
-  return result.data;
-   //const response = await fetch(`${API_BASE_URL}/ranking/tranco?domain=${encodeURIComponent(domain)}`);
-  //return response.json();
+const response = await fetch(`${API_BASE_URL}/ranking/tranco?domain=${encodeURIComponent(domain)}`);
+return response.json();
 
 }
 //for multiple domain
 export async function fetchmultdomain(domains){
-    const domainsArray = domains;
-    const result = await axios.get(api+"/multi",{
-        params:{
-            domains
-        },
-    });
-    return result.data;
-    //const response = await fetch(`${API_BASE_URL}/ranking/tranco/multi?domains=${encodeURIComponent(domains)}`);
-  //return response.json();
+const response = await fetch(`${API_BASE_URL}/ranking/tranco/multi?domains=${encodeURIComponent(domains)}`);
+return response.json();
 
 }
