aea9a67da1d3a6e20041118349ccfe3739d13a8b
Jayan Shrestha <jayanshr44@gmail.com>
2026-01-22 19:15:49 +1100

    updated button creating in input field and only fetches data if there are button input

--- src/views/DomainRanking.vue
+++ src/views/DomainRanking.vue
@@ -6,16 +6,18 @@
     </div>
     
     </div>
-    <div class="mx-auto max-w-6xl px-4 py-6 space-y-6 mt-5 border border-slate-200 bg-white p-4 shadow-sm">
-    <form @submit.prevent="onSubmit" class="form flex-col sm:flex-row">
-         <div
-         class="flex items-center flex-wrap gap-2 border rounded-3xl p-2 w-full cursor-text md:max-w-4xl xs:max-w-sm focus-within:ring-2 focus-within:ring-slate-400"
-    @click="focusInput"
+    <div class="mx-auto max-w-6xl px-4 py-6 space-y-6 mt-5 border rounded-3xl border-slate-200 bg-white p-4 shadow-sm">
+      <p class="px-8 -m-1 text-xs text-slate-700">Please press Enter to add domain. Click X to remove a domain.</p>
+      <form @submit.prevent="onSubmit" class="form flex-col sm:flex-row">
+      <div
+         class="flex items-center flex-wrap gap-2 border rounded-3xl p-2 w-full cursor-text md:max-w-4xl focus-within:ring-2 focus-within:ring-slate-400"
+    
 >
+<!--section for input button-->
       <div
         v-for="(item, index) in items"
         :key="index"
-        class="flex items-center gap-2 px-2 py-2 rounded-xl bg-slate-200 text-black text-sm font-medium"
+        class="flex items-center gap-2 px-2 py-2 rounded-xl bg-purple-200 text-slate-500 sm:text-base font-medium text-sm"
       >
         {{ item }}
 
@@ -36,22 +38,26 @@
         :placeholder="showPlaceholder?'Enter domain names like google.com or google.com,facebook.com' : ''" 
       />
     </div>
-     
+      
       <p v-if="!isValidDomain && input"><button class="text-black border-2 bg-amber-200 rounded-3xl  hover:translate-y-2  duration-300 cursor-pointer mt-3 sm:text-sm md:text-base text-xs" :disabled="!isValidDomain">❌ Invalid domain name</button></p>
-      <button v-if="!input || isValidDomain" class="border bg-gradient-to-r from-[#8c52ff] to-[#ff914d] text-gray-50 rounded-3xl font-sans font-semibold cursor-pointer h-fit sm:w-48 hover:translate-y-2 duration-300 mt-3 sm:text-sm md:text-base text-xs" :disabled="loading || !isValidDomain">
+      <button v-if="!input || isValidDomain" class="border bg-gradient-to-r from-[#8c52ff] to-[#ff914d] text-gray-50 rounded-3xl font-sans font-semibold cursor-pointer h-fit sm:w-48 hover:translate-y-2 duration-300 mt-3 sm:text-sm md:text-base text-xs">
       {{ loading ? 'Loading...' : 'Get Rankings' }} 
       </button>
       <button @click.stop="input='' || removeButtons()" type="button" class="border bg-[#ff914d] text-white rounded-3xl font-sans font-semibold cursor-pointer drop-shadow-sm h-fit hover:translate-y-2 duration-300 mt-3" ><span class="drop-shadow-md sm:text-sm md:text-base text-xs">Clear</span></button>
+    
     </form>
+   
     </div>
-    <p v-if="error" class="error max-w-6xl mx-auto my-5">{{"Type domain correctly and check again" }}</p>
+      
+    <p v-if="inputValue" class="error max-w-6xl mx-auto my-5 px-5">{{"Please enter a valid domain name or names separated by commas" }}</p>
+    <p v-if="error" class="error max-w-6xl mx-auto my-5 px-5">{{"Type domain correctly and check again" }}</p>
 
    
     <!-- Spinner -->
       <div v-if="loading" class="absolute inset-0 backdrop-blur-sm bg-white/40 z-20 flex flex-col justify-center items-center">
         <div class="flex border border-slate-500 border-opacity-40 shadow-sm bg-slate-200 p-3">
           <div class="animate-spin rounded-full h-10 w-10 border-4 border-blue-500 border-t-transparent"></div>
-        <p class="text-center text-slate-500 mt-4 text-sm pl-2">Fetching Domain Rankings...</p>
+        <p class="text-center text-slate-700 mt-4 text-sm pl-2">Fetching Domain Rankings...</p>
         </div>
         
       </div>
@@ -61,7 +67,7 @@
        <h2 class="text-md font-bold px-5 mb-5">Results</h2>
   
       <div class="flex justify-center items-center border-2  rounded-2xl border-slate-200 shadow-sm h-[60px] w-full px-5">
-        <p class="sm:text-sm  md:text-base text-xs  text-slate-400">Enter the domain names and search to view them in chart</p>
+        <p class="sm:text-sm  md:text-base text-xs  text-slate-700">Enter the domain names and search to view them in chart</p>
       
 </div>
 </div>
@@ -86,6 +92,7 @@ const isValidDomain = ref(false);
 const items = ref([]);
 const inputCleaned = ref('');
 const showPlaceholder = ref(true);// added boolean value to show placeholder
+const inputValue = ref(false);
 
 const multiDomainRegex =
   /^((?!:\/\/)([a-zA-Z0-9-]+\.)+[a-zA-Z]{2,6})(, (?!:\/\/)([a-zA-Z0-9-]+\.)+[a-zA-Z]{2,6})*$/;
@@ -135,11 +142,6 @@ showPlaceholder.value = true;
 
 }
 
-function focusInput() {
-  if (input.value) {
-    input.value.focus()
-  }
-}
 function removeButtons(){// function to remove all buttons when clear is clicked
   items.value.splice(0,items.value.length);
   showPlaceholder.value = true;
@@ -149,9 +151,24 @@ function removeButtons(){// function to remove all buttons when clear is clicked
 async function onSubmit() {
   error.value = '';
   results.value = [];
-  loading.value = true;
-
-  try {
+  if(items.value.length<1 && !input.value){
+   inputValue.value=true;
+   return;
+  }
+  if(items.value.length<1 && inputCleaned.value){// checks if there are no buttons and input field has value
+    items.value.push(inputCleaned.value.trim());// adds the input field value as button
+    input.value='';
+    showPlaceholder.value = false;
+  }
+  else{// if the items array has values
+     loading.value = true;
+    await fetchdata();
+  }
+ 
+ }
+  
+async function fetchdata(){
+    try {
     const domains = items.value.map(d => d.trim());//splits the strings into substrings and return them as array with white spaced removed
 
     if (domains.length === 1) {//if the domain is single domain input
@@ -174,7 +191,6 @@ async function onSubmit() {
     loading.value = false;
   
   }
-
 
 }
 
